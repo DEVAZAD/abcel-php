fe6f19353480f0ad423718265003b113c40d8043
DEVAZAD <deepanshu1701azad.com>
2025-11-06 14:31:42 +0530

    Add local PHP project files

---
+++ includes/header.php
@@ -0,0 +1,16 @@
+
+<header id="head">
+        <nav class="navbar">
+            <div class="container flex">
+                <div class="logo">
+                    <h1>ABCEL</h1>
+                </div>
+                <ul class="nav-links">
+                    <li><a href="/project">Home</a></li>
+                    <li><a href="/project/pages/about.php">About Us</a></li>
+                    <li><a href="/project/pages/programs.php">Programs</a></li>
+                    <li><a href="/project/pages/contact.php">Connect</a></li>
+                </ul>
+            </div>
+        </nav>
+    </header>
